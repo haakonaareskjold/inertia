4360baf4627810b1ecf992c8d93603a9c04b7954
haakonaareskjold <aareskjold.haakon@gmail.com>
2021-05-19 11:26:43 +0200

    added browser sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,7 +19,7 @@ mix.js('resources/js/app.js', 'public/js')
         require('autoprefixer'),
     ])
     .webpackConfig(require('./webpack.config'))
-    .browserSync('inertia.test');
+    .browserSync('http://127.0.0.1:8000');
 
 if (mix.inProduction()) {
     mix.version();
